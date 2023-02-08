b55f43addc622f131fe0b849729fbd9f83b30da9
Love <93451988+LoveOmoseebi@users.noreply.github.com>
2023-02-08 12:50:47 +0100

    SQL queries

--- unicorn companies.sql
+++ unicorn companies.sql
@@ -1,29 +1,29 @@
 -- The full unicorn companies table showing all details
 select *
-	from unicorn_companies
+	from unicorn_companies;
 
 
 -- Total number of unicorn companies in the dataset
 select count(*)
-	from unicorn_companies
+	from unicorn_companies;
 
 
 -- Different company industries in the table
 select industry, 
 	   count(*) as companies
 	from unicorn_companies
-	group by 1
+	group by 1;
 
 
 -- The Artificial intelligence industry is showing as two because of 'I' and 'i', we need to merge them.
 update unicorn_companies
 	set industry = 'Artificial intelligence'
-	where industry = 'Artificial Intelligence'
+	where industry = 'Artificial Intelligence';
 
 
 -- Total number of different industries of the unicorn companies
 select count(distinct industry)
-	from unicorn_companies
+	from unicorn_companies;
 
 
 -- Continents and their number of unicorn companies
@@ -31,7 +31,7 @@ select continent,
 	   count(*) as companies
 	from unicorn_companies
 	group by 1
-	order by 2 desc
+	order by 2 desc;
 
 
 -- countries and their number of unicorn companies
@@ -39,7 +39,7 @@ select country,
 	   count(*) as companies
 	from unicorn_companies
 	group by 1
-	order by 2 desc
+	order by 2 desc;
 
 
 -- average number of years it takes for a company to become a unicorn
@@ -47,7 +47,7 @@ select round(avg(yrs_to_unicorn),0) as avg_yrs_to_unicorn
 	from (
 		  select (year_joined - year_founded) as yrs_to_unicorn
 		  from unicorn_companies
-		 ) company_yrs
+		 ) company_yrs;
 		 
 		 
 -- top 5 countries on average to easily become a unicorn company
@@ -58,9 +58,10 @@ select country,
 				 (year_joined - year_founded) as yrs_to_unicorn
 		  from unicorn_companies
 		 ) company_yrs
-		 group by 1
-		 order by 2 ASC
-		 limit 5
+	group by 1
+	order by 2 ASC
+	limit 5;
+	
 		 
 -- top 5 continent on average to easily become a unicorn company
 select continent,
@@ -70,9 +71,9 @@ select continent,
 				 (year_joined - year_founded) as yrs_to_unicorn
 		  from unicorn_companies
 		 ) company_yrs
-		 group by 1
-		 order by 2 ASC
-		 limit 5
+	group by 1
+	order by 2 ASC
+	limit 5;
 
 
 -- average years it takes for companies in different industries to become unicorn
@@ -85,7 +86,7 @@ select industry,
 		  from unicorn_companies
 		 ) industry_yrs
 	group by 1
-	order by 2 asc
+	order by 2 asc;
 	
 	
 -- top 10 years with most conpanies becoming unicorn
@@ -95,62 +96,77 @@ select year_joined,
 	from unicorn_companies
 	group by 1
 	order by 2 desc
-	limit 10
+	limit 10;
 	
 	
 -- 	the top 5 companies with the biggest return on investment (ROI)
-select company, 
+with CTE_profit as
+		(
+			select company, 
+				   industry,
+				   funding,
+				   (valuation-funding) as profit
+			from unicorn_companies
+			where funding is not null
+		)
+select company,
 	   industry,
-	   (valuation - funding) as "ROI",
-	   rank() over(order by (valuation - funding) desc) as rnk
-	from unicorn_companies
+	   (profit/funding)*100 as "ROI",	   
+	   dense_rank() over(order by (profit/funding)*100 desc) as rnk
+	from CTE_profit
 	order by 3 desc
-	limit 5
+	limit 5;
 	   
 	   
 -- average return on investment of each industry
-with CTE_ROI as
+with CTE_industry_ROI as
 	(
 	  select industry,
-	   		 (valuation - funding) as ROI
-	  	 from unicorn_companies
+			 funding,
+	   		 (valuation - funding) as profit
+	  from unicorn_companies
+	  where funding is not null 
 	)
 select industry,
-	   round(avg(ROI),0) as "avg_ROI",
-	   rank() over(order by avg(ROI) desc) as rnk
-	from CTE_ROI
+	   round(avg(profit/funding)*100,0) as "avg_ROI",
+	   rank() over(order by avg(profit/funding)*100 desc) as rnk
+	from CTE_industry_ROI
 	group by 1
-	order by 2 desc
+	order by 2 desc;
 
 
--- top 5 countries with the best average ROI
-with CTE_ROI as
+-- top 5 countries with the best average ROI for companies
+with CTE_country_ROI as
 	(
 	  select country,
-	   		 (valuation - funding) as ROI
-	  	 from unicorn_companies
+			 funding,
+	   		 (valuation - funding) as profit
+	  from unicorn_companies
+	  where funding is not null 
 	)
 select country,
-	   round(avg(ROI),0) as "avg_ROI",
-	   rank() over(order by avg(ROI) desc) as rnk
-	from CTE_ROI
+	   round(avg(profit/funding)*100,0) as "avg_ROI",
+	   rank() over(order by avg(profit/funding)*100 desc) as rnk
+	from CTE_country_ROI
 	group by 1
 	order by 2 desc
-	limit 5
+	limit 5;
 	
 	
 -- top 5 continents with the best average ROI
-with CTE_ROI as
+with CTE_continent_ROI as
 	(
 	  select continent,
-	   		 (valuation - funding) as ROI
-	  	 from unicorn_companies
+			 funding,
+	   		 (valuation - funding) as profit
+	  from unicorn_companies
+	  where funding is not null 
 	)
 select continent,
-	   round(avg(ROI),0) as "avg_ROI",
-	   rank() over(order by avg(ROI) desc) as rnk
-	from CTE_ROI
+	   round(avg(profit/funding)*100,0) as "avg_ROI",
+	   rank() over(order by avg(profit/funding)*100 desc) as rnk
+	from CTE_continent_ROI
 	group by 1
 	order by 2 desc
-	limit 5
-	 
+	limit 5;
+	
